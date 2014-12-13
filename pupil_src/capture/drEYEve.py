7c057725f776247f14430618a0e26d12b98468c4
vdurrmann <vincent.durrmann@orange.fr>
2014-12-13 19:43:58 +0200

    Mode Control implemented with therehold befined as number of frames

--- pupil_src/capture/drEYEve.py
+++ pupil_src/capture/drEYEve.py
@@ -17,7 +17,7 @@ def on_resize(window,w, h):
     
 
 class drEYEve(Plugin):
-    
+    #State
     STOP = 0
     BACKWARD = 1
     NORMAL_SPEED = 2
@@ -30,7 +30,13 @@ class drEYEve(Plugin):
     UP_BORDER = 0.65
     DOWN_BORDER = 0.05
     #Threshold frames
-    FRAME_WAIT = 12
+    STATE_WAIT = 12 #minimum number of frame between 2 state changements
+    FORGET_BLINK = 30 #define when previous blinks have to be forgotten
+    BLINK = 25 #number of frame where the eye has to be closed to consider a vonlunteer blink
+    #Control mode
+    OFF = 0
+    ON = 1
+    
 
     def __init__(self,g_pool,atb_pos=(10,320)):
         Plugin.__init__(self)
@@ -42,7 +48,10 @@ class drEYEve(Plugin):
         self._window = None
         
         self.pupil_display_list = []
-        self.nb_frame = 0
+        self.nb_frame = 0 #to include a little delay between 2 changement of state
+        self.nb_opened = 0 #number of frame where the eye is opened
+        self.nb_closed = 0 #number of frame where the eye is closed
+        self.nb_blink = 0 #number of consecutive blink
         
         #To get the state Value
         def get_state(data):
@@ -51,8 +60,11 @@ class drEYEve(Plugin):
 
         #ATB variables
         self.state_enum = atb.enum("State",{"Stop":self.STOP, "Backward":self.BACKWARD,"Normal Speed":self.NORMAL_SPEED,"High Speed":self.HIGH_SPEED,"Left":self.LEFT,"Right":self.RIGHT})
+        self.control_mode_enum = atb.enum("Control Mode",{"OFF":self.OFF, "ON":self.ON})
         self.gaze_x = c_float(0.0)
         self.gaze_y = c_float(0.0)
+        self.state = c_int(0)
+        self.control_mode = c_int(0)
 
         # Creating an ATB Bar.
         atb_label = "drEYEve"
@@ -60,9 +72,10 @@ class drEYEve(Plugin):
             help="state of drEYEve project", color=(50, 50, 50), alpha=100,
             text='light', position=atb_pos,refresh=.3, size=(300, 100))
         
-        self.state_data = c_int(0)
         
-        self._bar.add_var("Current state", vtype=self.state_enum, getter=get_state, data=self.state_data, readonly=True)
+        
+        self._bar.add_var("Current state", vtype=self.state_enum, getter=get_state, data=self.state, readonly=True)
+        self._bar.add_var("Control mode", vtype=self.control_mode_enum, getter=get_state, data=self.control_mode, readonly=True)
         self._bar.add_var("Gaze X", self.gaze_x, readonly=True)
         self._bar.add_var("Gaze Y", self.gaze_y, readonly=True)
 
@@ -116,67 +129,98 @@ class drEYEve(Plugin):
     def update(self,frame,recent_pupil_positions,events):
         
         for pt in recent_pupil_positions:
+            #if the eye is detected
             if pt['norm_gaze'] is not None:
-                self.nb_frame = self.nb_frame + 1
-                if self.nb_frame > self.FRAME_WAIT: 
+                self.nb_frame += 1
+                self.nb_opened += 1
+                
+                #BLINK CONTROL
+                #if the eye is opened too long, previous blinks are forgotten
+                if(self.nb_opened > self.FORGET_BLINK):
+                    self.nb_blink = 0
+                #if the previous closed session was long, nb_blink is incremented
+                if(self.nb_closed > self.BLINK):
+                    self.nb_blink += 1
+                    print "Nb blink :"
+                    print self.nb_blink
+                #if there are 2 blinks : we switch the control mode (0->1 and 1->0)
+                if(self.nb_blink >= 2):
+                    if(self.control_mode.value == self.OFF):
+                        self.control_mode.value = self.ON
+                    else:
+                        self.control_mode.value = self.OFF
+                    self.nb_blink = 0
+                    print "Mode control"
+                    print self.control_mode.value
+                
+                self.nb_closed = 0
                     
+                #STATE CONTROL
+                if (self.nb_frame > self.STATE_WAIT and self.control_mode.value == self.ON): 
                     #self.LEFT
-                    if (pt['norm_gaze'][0] < self.LEFT_BORDER) and (self.state_data.value!=self.BACKWARD):
-                        self.previous_state = self.state_data.value
-                        self.state_data.value = self.LEFT
+                    if (pt['norm_gaze'][0] < self.LEFT_BORDER) and (self.state.value!=self.BACKWARD):
+                        self.previous_state = self.state.value
+                        self.state.value = self.LEFT
                         self.nb_frame = 0
-                    elif (self.state_data.value == self.LEFT) and (pt['norm_gaze'][0] > self.LEFT_BORDER):
+                    elif (self.state.value == self.LEFT) and (pt['norm_gaze'][0] > self.LEFT_BORDER):
                         if self.previous_state == self.STOP:
-                            self.state_data.value = self.STOP
+                            self.state.value = self.STOP
                             self.nb_frame = 0
                         else:
-                            self.state_data.value = self.NORMAL_SPEED
+                            self.state.value = self.NORMAL_SPEED
                             self.nb_frame = 0
                     #self.RIGHT
-                    elif (pt['norm_gaze'][0] > self.RIGHT_BORDER) and (self.state_data.value!=self.BACKWARD):
-                        self.previous_state = self.state_data.value
-                        self.state_data.value = self.RIGHT
+                    elif (pt['norm_gaze'][0] > self.RIGHT_BORDER) and (self.state.value!=self.BACKWARD):
+                        self.previous_state = self.state.value
+                        self.state.value = self.RIGHT
                         self.nb_frame = 0
-                    elif (self.state_data.value == self.RIGHT) and (pt['norm_gaze'][0] < self.RIGHT_BORDER):
+                    elif (self.state.value == self.RIGHT) and (pt['norm_gaze'][0] < self.RIGHT_BORDER):
                         if self.previous_state == self.STOP:
-                            self.state_data.value = self.STOP
+                            self.state.value = self.STOP
                             self.nb_frame = 0
                         else:
-                            self.state_data.value = self.NORMAL_SPEED
+                            self.state.value = self.NORMAL_SPEED
                             self.nb_frame = 0
                     #NORMAL SPEED
-                    elif (pt['norm_gaze'][1]>self.UP_BORDER) and (self.state_data.value==self.STOP):
-                        self.state_data.value = self.NORMAL_SPEED
+                    elif (pt['norm_gaze'][1]>self.UP_BORDER) and (self.state.value==self.STOP):
+                        self.state.value = self.NORMAL_SPEED
                         self.nb_frame = 0
-                    elif (pt['norm_gaze'][1]<self.DOWN_BORDER) and (self.state_data.value==self.HIGH_SPEED):
-                        self.state_data.value = self.NORMAL_SPEED
+                    elif (pt['norm_gaze'][1]<self.DOWN_BORDER) and (self.state.value==self.HIGH_SPEED):
+                        self.state.value = self.NORMAL_SPEED
                         self.nb_frame = 0
                     
                     #HIGH SPEED
-                    elif (pt['norm_gaze'][1]>self.UP_BORDER) and (self.state_data.value==self.NORMAL_SPEED):
-                        self.state_data.value = self.HIGH_SPEED
+                    elif (pt['norm_gaze'][1]>self.UP_BORDER) and (self.state.value==self.NORMAL_SPEED):
+                        self.state.value = self.HIGH_SPEED
                         self.nb_frame = 0
                     #self.BACKWARD
-                    elif (pt['norm_gaze'][1]<self.DOWN_BORDER) and (self.state_data.value==self.STOP):
-                        self.state_data.value = self.BACKWARD
+                    elif (pt['norm_gaze'][1]<self.DOWN_BORDER) and (self.state.value==self.STOP):
+                        self.state.value = self.BACKWARD
                         self.nb_frame = 0
                     
                     #self.STOP
-                    elif (pt['norm_gaze'][1]>self.UP_BORDER) and (self.state_data.value==self.BACKWARD):
-                        self.state_data.value = self.STOP
+                    elif (pt['norm_gaze'][1]>self.UP_BORDER) and (self.state.value==self.BACKWARD):
+                        self.state.value = self.STOP
                         self.nb_frame = 0
-                    elif (pt['norm_gaze'][1]<self.DOWN_BORDER) and (self.state_data.value==self.NORMAL_SPEED):
-                        self.state_data.value = self.STOP
+                    elif (pt['norm_gaze'][1]<self.DOWN_BORDER) and (self.state.value==self.NORMAL_SPEED):
+                        self.state.value = self.STOP
                         self.nb_frame = 0
                     
-                #END if nb_frame > FRAME_WAIT    
+                #END if nb_frame > STATE_WAIT    
                 
                 self.gaze_x.value = pt['norm_gaze'][0]
                 self.gaze_y.value = pt['norm_gaze'][1]
                    
                 
                 self.pupil_display_list.append(pt['norm_gaze'])
-            #END if pt
+            #if the eye is not detected
+            else:
+                self.nb_opened = 0
+                self.nb_closed += 1
+                #if the eye is closed too long, we stop
+                if self.nb_closed > self.BLINK:
+                    self.state.value = self.STOP
+                    print "Volunteer blink"
                 
         self.pupil_display_list[:-3] = []
 
